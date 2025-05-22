c4da28103c72cdfa51e04a8932d8b2059ef989da
Cormac Shannon <cormacpshannon@gmail.com>
2025-05-22 22:12:10 +0100

    Fix: Update /files/download to use new unwrapping

--- src/app/models/requests/__init__.py
+++ src/app/models/requests/__init__.py
@@ -1,3 +1,4 @@
-from .download_file import DownloadFile
+from .download_file import DownloadFileRequest
+from .signed_payload import SignedPayload
 
-__all__ = ["DownloadFile"]
+__all__ = ["DownloadFileRequest", "SignedPayload"]
--- src/app/models/requests/download_file.py
+++ src/app/models/requests/download_file.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 
 
-class DownloadFile(BaseModel):
+class DownloadFileRequest(BaseModel):
     uuid: str
 
 
