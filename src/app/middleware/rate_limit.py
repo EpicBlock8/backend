9e9f3c9972b3b3e554a2e9a0572660a897c8a98d
Cormac Shannon <cormacpshannon@gmail.com>
2025-06-04 11:41:55 +0100

    Fix: Clean up rate limit code

--- src/app/middleware/rate_limit.py
+++ src/app/middleware/rate_limit.py
@@ -1,18 +1,14 @@
 from collections import deque
 from time import monotonic
-from typing import TypeVar
 
 from fastapi import HTTPException, Request, Response
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 
-from app.models.requests import SignedPayload
 from app.shared import Config, Logger, load_config
 
 logger = Logger(__name__).get_logger()
 config: Config = load_config()
 config_rate_limit = config.network.rate_limit
-
-T = TypeVar("T")
 
 
 class RateLimit(BaseHTTPMiddleware):
@@ -35,7 +31,7 @@ class RateLimit(BaseHTTPMiddleware):
         self.__timeout_period_s = timeout_period_s
 
         # Checks
-        self.__ip: dict[str, deque[float]] = {}
+        self.__bucket: dict[str, deque[float]] = {}
         self.__timeout_club: dict[str, float] = {}
 
         # Time
@@ -54,39 +50,35 @@ class RateLimit(BaseHTTPMiddleware):
                 return await call_next(request)
 
             self.__now = monotonic()
-            self.__check_ip(request.client.host)
+            self.__check(request.client.host)
 
             return await call_next(request)
         except HTTPException as e:
             return Response(status_code=e.status_code)
 
-    def __check_ip(self, ip: str):
-        self.__check(self.__ip, ip, self.__max_per_second)
-
-    def __check(self, bucket: dict, key: str, limit: int):
+    def __check(self, key: str):
         # record the connection timestamp
         # if property is in timeout; then reject
         # lazyily prune old records
         # after pruning, if records exceeds
         # `max_per_second` then reject
 
-        self.__create_deque(bucket, key)
+        self.__create_deque(key)
         self.__timeout_check(key)
 
-        queue = bucket[key]
+        queue = self.__bucket[key]
         queue.append(self.__now)
 
         while self.__now - queue[0] > 1:
             queue.popleft()
 
-        if len(queue) > limit:
+        if len(queue) > self.__max_per_second:
             self.__timeout(key)
             raise HTTPException(status_code=429, detail="Too many requests.")
 
-    @staticmethod
-    def __create_deque(bucket: dict, key: str):
-        if key not in bucket:
-            bucket[key] = deque()
+    def __create_deque(self, key: str):
+        if key not in self.__bucket:
+            self.__bucket[key] = deque()
 
     def __timeout_check(self, key: str):
         if key not in self.__timeout_club:
